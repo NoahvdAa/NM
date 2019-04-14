2a961e98caf5d2f53f4824c844954d53b6705e43
Noah van der Aa <ndvdaa@gmail.com>
2019-04-14 17:06:31 +0200

    Added "fast login". When you have signed in once, this will remember your session token and auto log you in if possible.

--- index.js
+++ index.js
@@ -5,12 +5,47 @@ var schoolsByID = {};
 var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
+var cookieParser = require('cookie-parser');
 
 app.use(express.static('web/'));
+app.use(cookieParser());
+
+app.get('/set_session', async function (req, res){
+  if(typeof(req.query.schoolname) == 'undefined' || typeof(req.query.session) == 'undefined'){
+    res.send('?schoolname=<name>&session=<session id>');
+  }
+  res.cookie('schoolName', req.query.schoolname, {"maxAge": new Date(Date.now() + 86400000)});
+  res.cookie('session', req.query.session, {"maxAge": new Date(Date.now() + 86400000)});
+  res.send('OK');
+});
 
 app.ws('/magister', async function (ws, req) {
-  ws.send('{"type":"loginRequired","content":""}');
 
+  if(req.cookies){
+    if(typeof(req.cookies.schoolName) != 'undefined' && typeof(req.cookies.session)){
+      getSchools(req.cookies.schoolName).then(s=>{
+        if(s.length == 0){
+          ws.send('{"type":"loginRequired","content":""}');
+        }else{
+          magister({
+            school: s[0],
+            token: req.cookies.session
+          })
+          .then((m) => {
+            ws.send(`{"type":"fastlogin","content":"${req.cookies.schoolName}"}`);
+            ws.session = m;
+            ws.send(`{"type":"login_success","content":"${m.token}"}`);
+          }, (err) => {
+            // Login Failed
+            ws.send('{"type":"loginRequired","content":""}');
+          });
+        }
+      })
+    }
+  }else{
+    ws.send('{"type":"loginRequired","content":""}');
+  }
+  
   ws.on('message', async function (msg) {
     try {
       message = JSON.parse(msg);
@@ -48,7 +83,7 @@ app.ws('/magister', async function (ws, req) {
       })
       .then((m) => {
         ws.session = m;
-        ws.send(`{"type":"login_success","content":""}`);
+        ws.send(`{"type":"login_success","content":"${m.token}"}`);
       }, (err) => {
         ws.send(`{"error":"${err.toString()}"}`);
       });
