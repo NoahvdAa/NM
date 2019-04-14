ca69674de9b94384df66bc10af49681967333ede
Noah van der Aa <ndvdaa@gmail.com>
2019-04-14 10:28:23 +0200

    Added basic message handler.

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ app.use(express.static('web/'));
  
 app.ws('/magister', function(ws, req) {
   ws.on('message', function(msg) {
-    console.log(msg);
+    try {
+      var message = JSON.parse(msg);
+    } catch (e) {
+      return ws.send('Error: message must be JSON!');
+    }
+
+    if(typeof(message['type']) == 'undefined'){
+      return ws.send('Error: type is not specified.');
+    }else if(typeof(message['message']) == 'undefined'){
+      return ws.send('Error: message is not specified.');
+    }
   });
 });
  
 app.listen(80, function(){
-    console.log('Listening on port 80!');
+  console.log('Listening on port 80!');
 });
