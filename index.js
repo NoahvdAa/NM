6f29cf6d92ab00c923c53558b735565bb7de78a7
Noah van der Aa <ndvdaa@gmail.com>
2019-04-25 16:41:59 +0200

    Add ENV debug.

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ app.get('/set_session', async function (req, res) {
   res.send('OK');
 });
 
+console.log(process.env);
+
 app.ws('/magister', async function (ws, req) {
 
   ws.send(JSON.stringify({
@@ -149,5 +151,5 @@ app.ws('/magister', async function (ws, req) {
 });
 
 app.listen(process.env.PORT || 80, function () {
-  console.log('Listening on port 80!');
+  console.log('Listening on port '+(process.env.PORT || 80)+'!');
 });
